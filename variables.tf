1d5aa928e343d2ae35e1bd770d65a8f1d774296e
Sivasaivm <74296207+Sivasaivm@users.noreply.github.com>
2021-12-13 11:44:26 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,4 +3,9 @@
 #  description = "A description of my variable"
 #  default     = ""
 #}
+variable "myvariable" {
+  type        = string
+  description = "A description of my variable"
+  default     = ""
+}
 
