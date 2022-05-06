5252d9b8eaf995d916587dd8493e5c1db526a4a2
Sivasaivm <74296207+Sivasaivm@users.noreply.github.com>
2022-05-06 14:40:12 +0530

    modify variables.tf (#11)
    * modify variables.tf
    
    Signed-off-by: sivasaivm <sivasaivm@in.ibm.com>
    
    * update variables.tf
    
    Signed-off-by: sivasaivm <sivasaivm@in.ibm.com>

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ resource "aws_security_group" "ec2bastion" {
   vpc_id = var.vpc_id
 
   tags = {
-    Name = "${var.prefix_name}-bas-sg-group"
+    Name = "${var.name_prefix}-bas-sg-group"
   }
 }
 
